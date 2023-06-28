1f6ed594263dd27d1c4ff5d7eb85af27079e9b76
Palle95 <agustinpalleres2@gmail.com>
2023-06-28 20:50:39 -0300

    my last commit

--- examen_parcial_1.py
+++ examen_parcial_1.py
@@ -1,30 +1,43 @@
-#algoritmo para ingresar un numero entero 
-print("seleccione que accion desea realizar: 1.determinar si un numero es positivo o negativo \n 2. almacenar numeros para saber el resultado total de la sumatoria de los mismos ")
- 
+
+print("seleccione que accion desea realizar: \n 1. Determinar si un numero es positivo o negativo \n 2. Almacenar numeros para saber el resultado total de la sumatoria de los mismos \n 3. Determinar la suma de los digitos de un numero")
 op1 = 1
 op2 = 2
+op3 = 3
 op = int(input("ingrese la opcion: "))
 if op == 1:
-
+    print("Programa para determinar si un numero es positivo, negativo o igual a 0")
     num =  int(input("A continuacion ingrese un numero: "))
-
     if num < 0:
-        print("El numero ",num, " es negativo")
+        print("El numero ", num, " es negativo")
     elif num > 0:
         print("El numero ", num, " es positivo")
     elif num == 0:
         print("El numero ingresado es igual a: ",num)
+
 elif op == 2:
-    print("programa para calcular la sumatoria y el promedio")
-    
-    
+    print("Programa para calcular la sumatoria y el promedio de los numeros de una lista.")
     lista = []
     while True:
         numeros = int(input("a continuacion ingrese numeros: "))
         lista.append(numeros)
         if numeros == 0:
             break
-    
-print("numeros ingresados: ", lista)
+    print("numeros ingresados: ", lista) 
+    suma = sum(lista)
+    print("La sumatoria de los numeros de la lista es: ",suma)
+    cantidad = len(lista)
+    promedio = suma/cantidad
+    print("A continuacion se verá el promedio de los numeros de la lista: ",promedio)
+elif op == 3:
+    print("Programa para realizar la suma de los digitos de un numero entero")
+    ing_num = int(input("ingrese un numero entero con más de un digito "))
+    cad_num = str(ing_num)
+    suma_digit = 0
+
+    for digitos in cad_num:
+        suma_digit += int(digitos)
+
+    print("la suma de los digitos del numero 3",ing_num,"nos da un resultado de: ",suma_digit)
+ 
 
     
