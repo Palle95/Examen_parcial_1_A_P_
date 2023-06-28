c5d990d28fc9abc58ed8e76869c42942d8d85cb0
Palle95 <agustinpalleres2@gmail.com>
2023-06-28 19:15:41 -0300

    first commit

---
+++ examen_parcial_1.py
@@ -0,0 +1,10 @@
+x = "el valor de (a+b)*c es:"
+a,b,c = 4, 3, 2
+d = (a+b)*c 
+imprimir =  True 
+# comentarios con el numeral|
+if imprimir:
+    print(x, d)
+
+import keyword
+print(keyword.kwlist)
